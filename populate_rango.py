c43f4a09cbc04a417ac174b94cba0e2c06b2fa7f
Matthew Gallagher <2080573g@student.gla.ac.uk>
2020-02-11 17:05:16 +0000

    Ch.5 complete - models, population script, custom admin page

--- populate_rango.py
+++ populate_rango.py
@@ -20,14 +20,18 @@ def populate():
         {'title':'Bottle', 'url':'http://bottlepy.org/docs/dev/'},
         {'title':'Flask', 'url':'http://flask.pocoo.org'} ]
     
-    cats = {'Python': {'pages': python_pages},
-            'Django': {'pages': django_pages},
-            'Other Frameworks': {'pages': other_pages} }
+    cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
+            'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
+            'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16}
+            }
 
     for cat, cat_data in cats.items():
         c = add_cat(cat)
         for p in cat_data['pages']:
             add_page(c, p['title'], p['url'])
+        c.views = cat_data['views']
+        c.likes = cat_data['likes']
+        c.save()
     
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
@@ -40,8 +44,8 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name):
-    c = Category.objects.get_or_create(name=name)[0]
+def add_cat(name, views=0, likes=0):
+    c = Category.objects.get_or_create(name=name, views=views, likes=likes)[0]
     c.save()
     return c
 if __name__ == '__main__':
