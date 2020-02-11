220668c18244fe3fe73700e4dd54247f644ca983
Matthew Gallagher <2080573g@student.gla.ac.uk>
2020-02-11 18:12:59 +0000

    created slug field for categories

--- rango/views.py
+++ rango/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# Import the Category model
+from rango.models import Category
+
 def index(request):
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage matches to {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
-    # Return a rendered response to send to the client.
-    # Note that the first parameter is the template we wish to use.
+    # gets top 5 most liked categories
+    # Place the list in our context_dict dictionary that will be passed to the template engine.
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
+    context_dict['categories'] = category_list
+
+    # Return a rendered response to send to the client
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
