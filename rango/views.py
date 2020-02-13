40523b4de6eac36602470f1dd1662445e9ce8da0
Matthew Gallagher (student) <2080573G@student.gla.ac.uk>
2020-02-13 17:53:07 +0000

    Ch.6 complete - added top pages/cats to index

--- rango/views.py
+++ rango/views.py
@@ -1,19 +1,46 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-# Import the Category model
 from rango.models import Category
+from rango.models import Page
+
 
 def index(request):
     # gets top 5 most liked categories
     # Place the list in our context_dict dictionary that will be passed to the template engine.
     category_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
+    context_dict['pages'] = page_list
 
     # Return a rendered response to send to the client
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
     return render(request, 'rango/about.html')
+
+def show_category(request, category_name_slug):
+
+    context_dict = {}
+    try:
+        # The .get() method returns one model instance or raises a DoesNotExist exception.
+        category = Category.objects.get(slug=category_name_slug)
+
+        # Retrieve all of the associated pages.
+        pages = Page.objects.filter(category=category)
+
+        # Adds our results list to the template context under name pages.
+        context_dict['pages'] = pages
+
+        # We also add the category object from the database to the context dictionary.
+        # We'll use this in the template to verify that the category exists.
+        context_dict['category'] = category
+
+    except Category.DoesNotExist:
+        # the template will display the "no category" message for us.
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    # Go render the response and return it to the client.
+    return render(request, 'rango/category.html', context=context_dict)
